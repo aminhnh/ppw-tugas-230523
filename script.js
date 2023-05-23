bcd871fd59260dc4c024fa2048959f7614829e5e
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-23 20:05:06 +0700

    Add cekBilangan() function

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function cekBilangan() {
-    var bil1 = parseFloat(prompt("Bilangan pertama :"));
-    var bil2 = parseFloat(prompt("Bilangan kedua :"));
-    console.log(typeof bil1);
-    if (bil1 > bil2) {
-    }
+  var bil1 = parseFloat(prompt("Bilangan pertama :"));
+  var bil2 = parseFloat(prompt("Bilangan kedua :"));
+  var predikat;
+
+  if (bil1 > bil2) {
+    predikat = "lebih besar dari";
+  } else if (bil1 < bil2) {
+    predikat = "lebih kecil dari";
+  } else {
+    predikat = "sama dengan";
   }
+  var hasil = document.getElementById("hasil");
+  hasil.innerHTML = `Bilangan 1 = ${bil1}<br>Bilangan 2 = ${bil2}<br> Hasil : Bilangan ke-1 ${predikat} bilangan ke-2`;
+}
