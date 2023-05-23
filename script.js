43bbd98d0ef7248042271fcc36ce83dfb2b9cd33
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-23 19:54:40 +0700

    Move script to js file

---
+++ script.js
@@ -0,0 +1,7 @@
+function cekBilangan() {
+    var bil1 = parseFloat(prompt("Bilangan pertama :"));
+    var bil2 = parseFloat(prompt("Bilangan kedua :"));
+    console.log(typeof bil1);
+    if (bil1 > bil2) {
+    }
+  }
