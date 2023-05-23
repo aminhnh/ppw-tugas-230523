87aed6d60238f0f63d39a5c43aa8f7cb3941d1de
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-05-23 20:54:52 +0700

    Change var to let

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function cekBilangan() {
-  var bil1 = parseFloat(prompt("Bilangan pertama :"));
-  var bil2 = parseFloat(prompt("Bilangan kedua :"));
-  var predikat;
+  let bil1 = parseFloat(prompt("Bilangan pertama :"));
+  let bil2 = parseFloat(prompt("Bilangan kedua :"));
+  let predikat;
 
   if (bil1 > bil2) {
     predikat = "lebih besar dari";
@@ -10,17 +10,21 @@ function cekBilangan() {
   } else {
     predikat = "sama dengan";
   }
-  var hasil = document.getElementById("hasil");
+  let hasil = document.getElementById("hasil");
   hasil.innerHTML = `Bilangan 1 = ${bil1}<br>Bilangan 2 = ${bil2}<br> Hasil : Bilangan ke-1 ${predikat} bilangan ke-2`;
 }
 
-function data() {
-  var isPraktikan = confirm(
+function isiData() {
+  let isPraktikan = confirm(
     "Apakah anda praktikan mata kuliah Praktikum Pemrograman Web 1?"
   );
+  let no2 = document.getElementById("no-2");
   if (isPraktikan) {
-    nama = prompt("Nama : ");
-    nim = prompt("NIM : ");
-    angkatan = Number(prompt("Angkatan : "));
+    let nama = prompt("Nama : ", "Aminah Nurul Huda");
+    let nim = prompt("NIM : ", "22/499994/AM/30155");
+    let angkatan = Number(prompt("Angkatan : ", "22"));
+  } else {
+    no2.innerHTML = `<p>Anda bukan praktikan PPW1, anda tidak boleh
+    masuk</p>`;
   }
 }
